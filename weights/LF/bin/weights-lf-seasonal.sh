2b0e6c3d3e99fa866b7a871c3f3b81a555d68ee1
Reagan <rmckinney@eoas.ubc.ca>
2023-09-06 11:15:36 -0700

    added in weighted ensemmble algorithm

--- weights/LF/bin/weights-lf-seasonal.sh
+++ weights/LF/bin/weights-lf-seasonal.sh
@@ -1,10 +1,10 @@
-!/usr/bin/bash
+#!/usr/bin/bash
 
 source /home/verif/.bash_profile
 
 conda activate verification 
 
-cd /home/verif/verif-post-process/weights/LF
+cd /home/verif/verif-post-process/weights/LF/src
 
 k=(10 40 80 100 150 200 500 1000)
 

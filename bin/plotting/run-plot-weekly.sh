ac0387ff1a66d85df906a10d5b90a275553c860e
rmckinney@eoas.ubc.ca <rmckinney@eoas.ubc.ca>
2023-11-10 17:47:14 -0200

    changed cron and subsequent bin timies

--- bin/plotting/run-plot-weekly.sh
+++ bin/plotting/run-plot-weekly.sh
@@ -1,17 +1,17 @@
 source /home/verif/.bash_profile
 
-#start_date=`date --date="-10  days" +%y%m%d`
-#end_date=`date --date="-4 days" +%y%m%d`
+start_date=`date --date="-11  days" +%y%m%d`
+end_date=`date --date="-5 days" +%y%m%d`
 
-start_date='231027'
-end_date='231102'
+#start_date='231027'
+#end_date='231102'
 
 conda activate verification
 
 cd /home/verif/verif-post-process/src/
 
-#python3 leaderboards-plots.py $start_date $end_date small > log/lb_plots_sm.log
-#python3 leaderboards-plots.py $start_date $end_date large > log/lb_plots_lrg.log
+python3 leaderboards-plots.py $start_date $end_date small > log/lb_plots_sm.log
+python3 leaderboards-plots.py $start_date $end_date large > log/lb_plots_lrg.log
 python3 leaderboard-avg-ranking.py weekly small > log/lb_avg_ranking_sm.log
 python3 leaderboard-avg-ranking.py weekly large > log/lb_avg_ranking_lrg.log
 
--- bin/run-time-series.sh
+++ bin/run-time-series.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -l
-start_date='211015' #should not change, first day of stats calcs; must be the first of the month
-end_date=`date -d "-6 days" +%y%m%d`
+start_date='211001' #should not change, first day of stats calcs; must be the first of the month
+end_date=`date -d "-11 days" +%y%m%d`
 #end_date='231102'
 #echo $end_date
 source /home/verif/.bash_profile
@@ -9,18 +9,16 @@ conda activate verification
 
 cd /home/verif/verif-post-process/src
 
-#python3 time_series.py $start_date $end_date weekly small
-#python3 time_series.py $start_date $end_date weekly large
+python3 time_series.py $start_date $end_date weekly small
+python3 time_series.py $start_date $end_date weekly large
 
 #monthly date must start and end on first/end of month, so different end date given here
-start_month='211001'
-DATE=`date +%y%m%d`
-day=`date '+%d'`	
-echo $(day)
-end_month=$(date +%y%m%d -d "$DATE -8 days")
-echo $start_month
-echo $end_month
-python3 time_series.py $start_month $end_month monthly small
-python3 time_series.py $start_month $end_month monthly large
+#start_month='211001'
+#DATE=`date +%y%m%d`
+#day=`date '+%d'`	
+#end_month=$(date +%y%m%d -d "$DATE -8 days")
+
+python3 time_series.py $start_date $end_date monthly small
+python3 time_series.py $start_date $end_date monthly large
 
 	
